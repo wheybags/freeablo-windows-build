31bf59f0d43b74d2afc9ae52c2cd6ae47f59b602
Tom Mason <wheybags@wheybags.com>
2015-05-14 23:12:38 +0100

    add qt

--- cmake.bat
+++ cmake.bat
@@ -1,10 +1,23 @@
+@echo off
 
 :http://stackoverflow.com/questions/3551888/pausing-a-batch-file-when-double-clicked-but-not-when-run-from-a-console-window
 for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
 
+
+IF EXIST Qt GOTO QTINSTALLED
+    echo downloading qt, this may take a while (~600mb installer)...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('http://download.qt.io/official_releases/qt/5.4/5.4.1/qt-opensource-windows-x86-msvc2010_opengl-5.4.1.exe', 'qt-opensource-windows-x86-msvc2010_opengl-5.4.1.exe')"
+       
+    echo executing qt installer, please select %CD%\Qt\Qt5.4.1\ as the installation directory
+    pause
+    start /WAIT qt-opensource-windows-x86-msvc2010_opengl-5.4.1.exe
+    
+:QTINSTALLED
+
 IF NOT EXIST build\ GOTO NOBUILDFOLDER
     rmdir /s /q build\
 :NOBUILDFOLDER
+
 
 mkdir build
 
@@ -17,6 +30,13 @@ copy deps\Python27\python27_d.dll build\Debug\
 copy deps\libRocket\lib\RocketCore_d.dll build\Debug
 copy deps\libRocket\lib\RocketDebugger_d.dll build\Debug
 copy deps\libRocket\lib\RocketControls_d.dll build\Debug
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\Qt5Widgetsd.dll build\Debug
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\Qt5Guid.dll build\Debug
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\Qt5Cored.dll build\Debug
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\icudt53.dll build\Debug
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\icuin53.dll build\Debug
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\icuuc53.dll build\Debug
+
 
 
 mkdir build\Release
@@ -28,6 +48,12 @@ copy deps\Python27\python27.dll build\Release
 copy deps\libRocket\lib\RocketCore.dll build\Release
 copy deps\libRocket\lib\RocketDebugger.dll build\Release
 copy deps\libRocket\lib\RocketControls.dll build\Release
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\Qt5Widgets.dll build\Release
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\Qt5Gui.dll build\Release
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\Qt5Core.dll build\Release
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\icudt53.dll build\Release
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\icuin53.dll build\Release
+copy Qt\Qt5.4.1\5.4\msvc2010_opengl\bin\icuuc53.dll build\Release
 
 copy deps\libRocket\lib\*.pyd build\
 copy deps\libRocket\lib\rocket.py build\
@@ -74,6 +100,9 @@ cd ..\windows-include\
 set WIN_INCLUDE=%CD%
 cd %CURRDIR%
 
+cd ..\Qt\Qt5.4.1\5.4\msvc2010_opengl
+set CMAKE_PREFIX_PATH=%CD%
+cd %CURRDIR%
 
 cmake.exe  -G "Visual Studio 10" ..\freeablo -DCLI_INCLUDE_DIRS=%WIN_INCLUDE% -DPYTHON_INCLUDE_DIR=%PYTHON_INCLUDE_DIR% -DPYTHON_LIBRARY=%PYTHON_LIBRARY% -DPYTHON_DEBUG_LIBRARY=%PYTHON_DEBUG_LIBRARY% -DBoost_USE_STATIC_LIBS=On
 
